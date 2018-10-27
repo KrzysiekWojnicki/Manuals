90f47438af6d672a6f567d24ba6d9c75719e1a81
Krzyszto W <krzysiek_wojnicki@yahoo.com>
2018-10-27 23:37:11 +0200

    Upgrading script "now_plik.sh" , creating new directory and changing pasting there all "how to files"

--- nowe_pliki.sh
+++ nowe_pliki.sh
@@ -2,6 +2,27 @@
 
 #short script to make test files
 
+
+funkcja_p_ile()
+{
+
+	echo "wpisz ile chcesz plików: "
+	read liczba1
+	for (( c=1 ; c<=$liczba1 ; c++))
+	do
+		r=$(( $RANDOM % 2 ))
+		case $r in
+			  "0") ko='txt';;
+			  "1") ko='end';;
+		   	  *) echo coś poszło nie tak
+		esac
+		ra=$(head /dev/urandom | tr -dc A-Za-z | head -c 6)
+	touch $ra.$ko
+	done
+}
+
+	echo "Witaj , ile chcesz plików: "
+funkcja_p_ile
 funkcja_czy_jeszcze()
 {
 echo "chcesz utworzyć jeszcze? [t/n]:"
@@ -11,24 +32,12 @@ then
 funkcja_p_ile
 fi
 }
-funkcja_p_ile()
-{
-
-	echo "wpisz ile chcesz plików: "
-	read liczba1
-	for (( c=1 ; c<=$liczba1 ; c++))
-	do
-		touch plik$c
-	done
-}
-
-	echo "Witaj , ile chcesz plików: "
-funkcja_p_ile
 funkcja_czy_jeszcze
-
-echo utworzono $liczba1 plików.
+sum_p=$(ls *.txt *.end | wc -w)
+echo utworzono $sum_p plików.
 
 
 # w następnej wersji można by zastosować zew.
 # funkce np; source ~/naszplik_z_funkcjami
 # np; na inne możliwe pliki do tworzenia
+
