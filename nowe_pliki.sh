e60202cd5155d677c880f5f2d47327ba45e283a0
Krzyszto W <krzysiek_wojnicki@yahoo.com>
2018-10-28 23:42:05 +0100

    code correcting

--- nowe_pliki.sh
+++ nowe_pliki.sh
@@ -3,11 +3,15 @@
 #short script to make test files
 
 
+	echo "Witaj , ile chcesz plików: "
+	
 funkcja_p_ile()
 {
 
 	echo "wpisz ile chcesz plików: "
 	read liczba1
+	l=0
+	suma[l]=$liczba1
 	for (( c=1 ; c<=$liczba1 ; c++))
 	do
 		r=$(( $RANDOM % 2 ))
@@ -19,23 +23,37 @@ funkcja_p_ile()
 		ra=$(head /dev/urandom | tr -dc A-Za-z | head -c 6)
 	touch $ra.$ko
 	done
+	funkcja_czy_jeszcze
 }
 
-	echo "Witaj , ile chcesz plików: "
-funkcja_p_ile
 funkcja_czy_jeszcze()
 {
 echo "chcesz utworzyć jeszcze? [t/n]:"
 read pytanie
 if [[ $pytanie =~ [yYTt] ]]
+	then
+	funkcja_p_ile  
+elif [[ $pytanie =~ [nN] ]]
 then
-funkcja_p_ile
+	# zliczanie
+	n_zlicz
+else
+	echo '[t/n]?'
+	funkcja_czy_jeszcze
+
 fi
 }
-funkcja_czy_jeszcze
+
+zliczanie(){
 sum_p=$(ls *.txt *.end | wc -w)
 echo utworzono $sum_p plików.
+}
+n_zlicz(){
+	echo "${suma[*]}" liczb
+}
+###### CODE #########
 
+funkcja_p_ile
 
 # w następnej wersji można by zastosować zew.
 # funkce np; source ~/naszplik_z_funkcjami
