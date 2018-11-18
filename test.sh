8c956c6d9e3904277beeb24edf29fafbcf19cc19
Krzyszto W <krzysiek_wojnicki@yahoo.com>
2018-11-18 23:13:23 +0100

    slightly changing code

--- test.sh
+++ test.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-# if (($# == 0)); then
-# 	echo -e "nie podano żadnej op\n -h --hepl"
-if ( ! getopts ":f:xh" opcja )
-then echo "funkcja $0 nie przyjmuje ($1)  ,spr -h"
-else 
+
+
+
+if (($# == 0)); then
+	echo -e "nie podano żadnej opcji, spróbuj -h --hepl"
+elif ( ! getopts ":f:xh" opcja )
+	then echo "nieprawidłowa opcja funkcji $0 , spróbuj -h"
+	fi
+
 
 while getopts ":f:xh" opcja;
 do
 	case $opcja in
 		h) echo -e "help\nf -tworzenie pliku\nh -help";; 
-		f) echo $OPTARG ;;
-		\?) echo nieprawidłowa op -$OPTARG ;; 
+		f) echo wybrałeś -f z parametrem $OPTARG ;;
+		\?) echo nieprawidłowa opcja- $OPTARG ;; 
 		:) echo opcja -$OPTARG potrzebuje argumentu;;
 	esac
 done
-fi
