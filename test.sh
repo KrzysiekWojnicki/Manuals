31a2b6296a52af456e3cc3ac7d9c02a7a7ee9824
Krzyszto W <krzysiek_wojnicki@yahoo.com>
2018-11-24 23:55:18 +0100

    adding little content

--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-
-
+NORM=`tput sgr0`
+BOLD=`tput bold`
+REV=`tput smso`
+    
+# funkcja sprawdzająca 
+function SPR {
 if (($# == 0)); then
 	echo -e "nie podano żadnej opcji, spróbuj -h --hepl"
 elif ( ! getopts ":f:xh" opcja )
@@ -12,9 +16,27 @@ elif ( ! getopts ":f:xh" opcja )
 while getopts ":f:xh" opcja;
 do
 	case $opcja in
-		h) echo -e "help\nf -tworzenie pliku\nh -help";; 
+		h) HELP ;;
 		f) echo wybrałeś -f z parametrem $OPTARG ;;
 		\?) echo nieprawidłowa opcja- $OPTARG ;; 
 		:) echo opcja -$OPTARG potrzebuje argumentu;;
 	esac
 done
+}
+
+# funkcja help
+function HELP {
+echo -e \\n"Help documentation for ${BOLD}${SCRIPT}.${NORM}"\\n
+echo -e "${REV}Basic usage:${NORM} ${BOLD}$SCRIPT file.ext${NORM}"\\n
+echo "Command line switches are optional. The following switches are recognized."
+echo "${REV}-a${NORM}  --Sets the value for option ${BOLD}a${NORM}. Default is ${BOLD}A${NORM}."
+echo "${REV}-b${NORM}  --Sets the value for option ${BOLD}b${NORM}. Default is ${BOLD}B${NORM}."
+echo "${REV}-c${NORM}  --Sets the value for option ${BOLD}c${NORM}. Default is ${BOLD}C${NORM}."
+echo "${REV}-d${NORM}  --Sets the value for option ${BOLD}d${NORM}. Default is ${BOLD}D${NORM}."
+echo -e "${REV}-h${NORM}  --Displays this help message. No further functions are performed."\\n
+echo -e "Example: ${BOLD}$SCRIPT -a foo -b man -c chu -d bar file.ext${NORM}"\\n
+exit 1
+}
+
+# Właściwy Start Kodu
+SPR
